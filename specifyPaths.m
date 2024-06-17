9f16f4b7405eb3b6b4e989c74624066d82facde4
brandonhackney <65794862+brandonhackney@users.noreply.github.com>
2024-06-17 12:37:23 -0700

    Make specifyPaths more dynamic

--- specifyPaths.m
+++ specifyPaths.m
@@ -1,15 +1,15 @@
 function p = specifyPaths
 
 %relevant paths
-p.root = '/data2/2020_STS_Multitask/';
-p.basePath = strcat(p.root, 'analysis/');
-p.baseDataPath = strcat(p.root, 'data/');
-p.backup = strcat(p.root, 'backup/');
-p.deriv = strcat(p.baseDataPath, 'deriv/');
-p.nullDir = strcat(p.baseDataPath, 'sub-04/fs/sub-04/label/');
-p.template = strcat(p.baseDataPath, 'sub-04/fs/sub-04-Surf2BV/');
+p.basePath = fileparts(which('specifyPaths.m'));
+p.root = fileparts(p.basePath); % up one level
+p.baseDataPath = fullfile(p.root, 'data');
+p.backup = fullfile(p.root, 'backup');
+p.deriv = fullfile(p.baseDataPath, 'deriv');
+p.nullDir = fullfile(p.baseDataPath, 'sub-04','fs','sub-04','label');
+p.template = fullfile(p.baseDataPath, 'sub-04','fs','sub-04-Surf2BV');
 
 % p.wbFCPath = strcat(p.basePath, 'wholebrainFC/');
 % p.corrOutPath = strcat(p.wbFCPath, 'corrData/');
-p.classifyPath = strcat(p.basePath, 'class/libsvm_scripts/');
-p.classifyDataPath = strcat(p.basePath, 'class/data/');
+p.classifyPath = fullfile(p.basePath, 'class','libsvm_scripts');
+p.classifyDataPath = fullfile(p.basePath, 'class','data');
